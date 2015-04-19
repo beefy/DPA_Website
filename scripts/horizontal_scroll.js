ac2c278b2f4cfdbbdcc9a280743d0a4e163a8ea8
unknown <nate.schultz@outlook.com>
2015-04-18 21:56:25 -0400

    tried to implement horizontal scrolling

---
+++ scripts/horizontal_scroll.js
@@ -0,0 +1,17 @@
+﻿function scroll() {
+    //var scrolled = $(window).scrollTop();
+    //$('#wrapper').css('left', (0 - (scrolled * .9)) + 'px');
+}
+
+function scroll2() {
+    var scrolled = $(window).scrollTop();
+    $('#wrapper').css('left', (0 - (scrolled * .9)) + 'px');
+}
+
+$(document).ready(function () {
+
+    /* Scroll event handler */
+    $(window).bind('scroll', function (e) {
+        scroll2();
+    });
+});
